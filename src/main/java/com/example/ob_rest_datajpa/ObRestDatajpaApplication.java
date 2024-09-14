2c71d24e0433676d2f04b6d33722555bf53ec031
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2024-09-14 10:36:12 -0500

    Initial commit

---
+++ src/main/java/com/example/ob_rest_datajpa/ObRestDatajpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ob_rest_datajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObRestDatajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObRestDatajpaApplication.class, args);
+	}
+
+}
