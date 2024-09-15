0bc59900da090ad3d859c4c9cb2f6779b8bee604
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2024-09-14 19:21:16 -0500

    add file Spring

--- src/main/java/com/example/ob_rest_datajpa/controller/BookController.java
+++ src/main/java/com/example/ob_rest_datajpa/controller/BookController.java
@@ -2,15 +2,19 @@ package com.example.ob_rest_datajpa.controller;
 
 import com.example.ob_rest_datajpa.entities.Book;
 import com.example.ob_rest_datajpa.repository.BookRepository;
-import jakarta.persistence.Id;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class BookController {
 
+    private static final Logger log = LoggerFactory.getLogger(BookController.class);
     //Atributos
     private BookRepository bookRepository;
 
@@ -34,8 +38,87 @@ public class BookController {
         return bookRepository.findAll();
     }
 
-//    public Book findOneById(Long id){
-//        return  bookRepository.findById(id);
-//    }
+    /**
+     * Devuelve un libro con el parámetro ID
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("api/books/{id}")
+    public ResponseEntity<Book> findOneById(@PathVariable Long id) {
+        Optional<Book> bookOpt = bookRepository.findById(id);
+//         if (bookOpt.isPresent())
+//            return ResponseEntity.ok(bookOpt.get());
+//        else
+//            return ResponseEntity.notFound().build();
+        return bookOpt.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
 
+    /**
+     * Crear un nuevo libro en base de datos
+     *
+     * @param book
+     * @return new book
+     */
+    @PostMapping("/api/books")
+    public ResponseEntity<Book> create(@RequestBody Book book) {
+        //Guardar el libro recibido por parámetro en la base de datos
+        if (book.getId() != null) {
+            //Quiere decir que existe el id y por lo tanto no es una creacion
+            log.warn("trying to crate a book by Id");
+            System.out.println("trying to crate a book by Id");
+            return ResponseEntity.badRequest().build();
+        }
+        Book result = bookRepository.save(book);
+        return ResponseEntity.ok(result);
+    }
+
+    /**
+     * Metodo UPDATE para actualizar un libro
+     *
+     * @param book
+     * @return
+     */
+    @PutMapping("/api/books")
+    public ResponseEntity<Book> update(@RequestBody Book book) {
+        if (book.getId() == null) {
+            log.warn("Trying to update a non existent book");
+            return ResponseEntity.badRequest().build(); //400
+        }
+        if (!bookRepository.existsById(book.getId())) {
+            log.warn("Trying to update a non existe book");
+            return ResponseEntity.notFound().build();//404
+        }
+        //Proceso de Actualizacion
+        Book result = bookRepository.save(book);
+        return ResponseEntity.ok(result);
+    }
+
+    /**
+     * Metodo DELETE para eliminar un libro
+     *
+     * @param id
+     * @return
+     */
+    @DeleteMapping("/api/books/{id}")
+    public ResponseEntity<Book> delete(@PathVariable Long id) {
+        if (bookRepository.existsById(id)) {
+            log.warn("Trying to delete a non existent book");
+            return ResponseEntity.notFound().build();//404
+        }
+        bookRepository.deleteById(id);
+        return ResponseEntity.noContent().build();//2xx
+    }
+
+    @DeleteMapping("/api/books")
+    public ResponseEntity<Book> deleteAll() {
+        List<Book> books = bookRepository.findAll();
+        if (books.isEmpty()) {
+            log.warn("Trying to delete a non existent books");
+            return ResponseEntity.badRequest().build();
+        }
+        log.info("REST Request for delete all books");
+        bookRepository.deleteAll();
+        return ResponseEntity.noContent().build();
+    }
 }
